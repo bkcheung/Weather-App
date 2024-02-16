12d406faa4b11a100a09cb5c5e33ab2c6e886a65
Bonnie Cheung <b.cheung@hotmail.com>
2024-02-16 11:19:19 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+import { pageInit } from "./init";
+import { storageAvailable } from "./storage";
+
+//Check for existing data from storage
+if (storageAvailable("localStorage")) {
+  console.log("Local storage is available!");
+}
+pageInit();
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Wot To Do",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "./",
+  },
+  optimization: {
+    minimize: true,
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+};
