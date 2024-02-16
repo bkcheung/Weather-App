5bfd247627c3c2f287801bb84adeb1e1d59d9e49
bkcheung <b.cheung@hotmail.com>
2024-02-16 11:26:48 -0500

    Remove old files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import { pageInit } from "./init";
-import { storageAvailable } from "./storage";
-
-//Check for existing data from storage
-if (storageAvailable("localStorage")) {
-  console.log("Local storage is available!");
-}
-pageInit();
