977178f3b6fe68fc75d5c9db6076a45c1150b03d
bkcheung <b.cheung@hotmail.com>
2024-02-19 15:09:07 -0500

    Add location searching and refresh

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import "./style.css"
 const dayjs = require('dayjs')
 
+searchInit();
 getWeather();
 
-async function getWeather(){
+async function getWeather(location){
     try{
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=a5a72b44830a4c4d99e135309241502&q=Toronto&days=3&aqi=yes&alerts=yes`);
+        clear();
+        if(location===undefined){
+            location = "Toronto";
+        }
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json
+                                      ?key=a5a72b44830a4c4d99e135309241502&q=
+                                      ${location}&days=3&aqi=yes&alerts=yes`);
         const weather = await response.json();
         renderCurrent(weather);
         renderForecast(weather.forecast.forecastday);
@@ -14,25 +21,25 @@ async function getWeather(){
     }
 }
 
+function searchInit(){
+    const search = document.getElementById("search");
+    search.addEventListener('keyup', (e)=>{
+        if(e.key==="Enter"){
+            getWeather(e.target.value);
+        }
+    })
+}
+
 function renderCurrent(weather){
-    const page = document.getElementById("weather");
     const location = document.getElementById("location");
     location.innerHTML = weather.location.name;
-    const curr = document.createElement("div");
-    curr.classList.add('current');
-    const title = document.createElement("div");
-    title.innerHTML = "Current Conditions";
-    const icon = document.createElement("img");
+
+    const icon = document.getElementById("icon");
     icon.src = weather.current.condition.icon;
-    const condition = document.createElement("div");
+    const condition = document.getElementById("condition");
     condition.innerHTML = weather.current.condition.text;
-    const temp = document.createElement("div");
+    const temp = document.getElementById("temp");
     temp.innerHTML = `${weather.current.temp_c}°C`;
-    curr.appendChild(title);
-    curr.appendChild(icon);
-    curr.appendChild(condition);
-    curr.appendChild(temp);
-    page.appendChild(curr);
 }
 
 function renderForecast(forecast){
@@ -40,13 +47,14 @@ function renderForecast(forecast){
     for(let i=0; i<3; i++){
         const date = document.createElement('div');
         date.innerHTML = dayjs(forecast[i].date).format('ddd MMM D');
+        date.classList.add("title");
         const condition = document.createElement('div');
         condition.innerHTML = forecast[i].day.condition.text;
         const icon = document.createElement('img');
         icon.src = forecast[i].day.condition.icon;
         const temp = document.createElement('div');
-        temp.innerHTML = `${Math.round(forecast[i].day.maxtemp_c)}°C / ${Math.round(forecast[i].day.mintemp_c)}°C`;
-        console.log(forecast[i].day);
+        temp.innerHTML = `${Math.round(forecast[i].day.maxtemp_c)}°C / 
+                          ${Math.round(forecast[i].day.mintemp_c)}°C`;
 
         const dayTile = document.createElement('div');
         dayTile.classList.add('dayTile');
@@ -58,5 +66,12 @@ function renderForecast(forecast){
     }
 }
 
-
-
+function clear(){
+    const weather = document.getElementById("weather");
+    const rm = weather.querySelectorAll('div.dayTile');
+    if(rm.length>0){
+        for(let i=0; i<3; i++){
+            rm[i].remove();
+        }
+    }
+}
