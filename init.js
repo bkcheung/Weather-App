b96ab82a6bafe6facc9d09770fb00aa3cef2015d
bkcheung <b.cheung@hotmail.com>
2024-02-20 11:47:26 -0500

    Adjust search box and tile widths in narrow mode

--- init.js
+++ init.js
@@ -4,27 +4,31 @@ export function pageInit() {
   resizeHandling();
 }
 function initDisplayType() {
-  const weather = document.getElementById("weather");
   const page = document.getElementById("page");
+  const weather = document.getElementById("weather");
+  const location = document.getElementById("location");
   const current = document.getElementById("current");
   const resultDrop = document.getElementById("resultDrop");
   if (window.innerWidth < threshold) {
-    weather.classList.add("narrow");
     page.classList.add("narrow");
+    weather.classList.add("narrow");
+    location.classList.add("narrow");
     current.classList.add("narrow");
     resultDrop.classList.add("narrow");
   }
 }
 function resizeHandling() {
-  const weather = document.getElementById("weather");
   const page = document.getElementById("page");
+  const weather = document.getElementById("weather");
+  const location = document.getElementById("location");
   const current = document.getElementById("current");
   const resultDrop = document.getElementById("resultDrop");
   window.addEventListener("resize", () => {
     if (window.innerWidth < threshold) {
       const dayTiles = document.querySelectorAll("div.dayTile");
-      weather.classList.add("narrow");
       page.classList.add("narrow");
+      weather.classList.add("narrow");
+      location.classList.add("narrow");
       current.classList.add("narrow");
       resultDrop.classList.add("narrow");
       dayTiles.forEach((item) => {
@@ -33,8 +37,9 @@ function resizeHandling() {
     }
     if (window.innerWidth > threshold) {
       const dayTiles = document.querySelectorAll("div.dayTile");
+      page.classList.add("narrow");
       weather.classList.remove("narrow");
-      page.classList.remove("narrow");
+      location.classList.remove("narrow");
       current.classList.remove("narrow");
       resultDrop.classList.remove("narrow");
       dayTiles.forEach((item) => {
